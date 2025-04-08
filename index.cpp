4d1b4a7966ffba6d362a767bd577d43a7225f834
diegoodelatorre <diegoalejandrodelgadodelatorre@gmail.com>
2025-04-07 21:14:12 -0600

    Segundo commit

--- index.cpp
+++ index.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main(){
-    
+    int num1 = 5, num2 = 3;
+    int suma = num1 + num2;
+    cout << "La suma de " << num1 << " y " << num2 << " es: " << suma << endl;
     return 0;
 };
